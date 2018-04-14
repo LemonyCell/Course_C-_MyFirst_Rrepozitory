2da00550c7402c3273d04ebd2647a5b31f711952
Vitya80100 <misterstop7@gmail.com>
2018-04-14 04:57:03 +0300

    added a Room class

---
+++ ConsoleApp_lesson2/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_lesson2
+{
+    class Client : Human
+    {
+
+        public Place Place { get; set; }
+
+    }
+}
