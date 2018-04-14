fd505b7ff05d0dad315182a92363f0ec493a6069
Vitya80100 <misterstop7@gmail.com>
2018-04-14 15:11:37 +0300

    added several methods to the God class. created designer Human, Restaurant, Toilet, Owner, Hall. added myRand class

---
+++ ConsoleApp_lesson2/MyRand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_lesson2
+{
+    class MyRand
+    {
+        public static Random r = new Random();
+
+        public static int RandNum(int minVal, int MaxVal)
+        {
+            return (r.Next(minVal, MaxVal));
+        }
+
+        public static string RandSrt(int len)
+        {
+
+            string s = "";
+            for (int i = 0; i < len; i++)
+            {
+                s += (char)r.Next('a', 'z' + 1);
+            }
+
+            return s;
+        }
+
+        public static string RandSrtUpper(int len)
+        {
+
+            string s = "";
+            for (int i = 0; i < len; i++)
+            {
+                s += (char)r.Next('a', 'z' + 1);
+            }
+
+            return s;
+        }
+
+    }
+}
