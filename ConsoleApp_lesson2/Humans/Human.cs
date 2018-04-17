3e86edf10470968f63a8cbde48580fc95b3c2cf5
Vitya80100 <32651974+Vitya80100@users.noreply.github.com>
2018-04-17 04:57:33 +0300

    Update Human.cs

--- ConsoleApp_lesson2/Humans/Human.cs
+++ ConsoleApp_lesson2/Humans/Human.cs
@@ -15,23 +15,6 @@ namespace ConsoleApp_lesson2
         public string LastName { get; set; }
         public int Age { get; set; }
         public Gender Sex { get; set; }
-
-        //public Human()
-        //{
-        //    CreateHuman();
-        //}
-
-        //public void CreateHuman()
-        //{
-
-            
-        //    this.FirstName = MyRand.RandSrtUpper(MyRand.RandNum(4, 12));
-        //    this.LastName = MyRand.RandSrtUpper(MyRand.RandNum(4, 12));
-        //    this.Age = MyRand.RandNum(18, 65);
-        //    this.Sex = (Gender)MyRand.RandNum(0, 1);
-        //    this.Id = Convert.ToString(God.NumOfHuman);
-
-        //}
-        
+		
     }
 }
